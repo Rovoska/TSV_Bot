f43483e3cae1fab2177594761ab956931cf378dc
Sturmrufer <theskyserpent@gmail.com>
2016-09-06 15:52:46 -0400

    Update TSV_Bot.py

--- TSV_Bot.py
+++ TSV_Bot.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
-import praw
+import praw 
 
 user_agent = ("TSV Bot 0.1")
 r = praw.Reddit(user_agent=user_agent)
 subreddit = r.get_subreddit("svexchange")
 tsv = '1132'
 
+# Get posts and filter if containing 1132
 for submission in subreddit.get_new(limit=1):
     if re.search('1132', submission.selftext, re.IGNORECASE):
         print ('Title:'), submission.title

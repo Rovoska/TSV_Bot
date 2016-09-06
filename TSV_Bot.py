94d671bd5d6bec683c3b575086e4ca52ed2a5b7d
Sturmrufer <theskyserpent@gmail.com>
2016-09-05 21:57:47 -0400

    Update TSV_Bot.py

--- TSV_Bot.py
+++ TSV_Bot.py
@@ -1,9 +1,17 @@
+#!/usr/bin/python
 import praw
 
 user_agent = ("TSV Bot 0.1")
-r = praw.reddit(user_agent = user_agent)
-subreddit = praw.get_subreddit("svexhange")
+r = praw.Reddit(user_agent=user_agent)
+subreddit = r.get_subreddit("svexchange")
+tsv = '1132'
 
-for submission in subreddit.get_new(limit = 50):
-	print ('Body: '), submission.selftext
-	
+for submission in subreddit.get_new(limit=1):
+    op_text = submission.selftext
+    tsv_match = any(string in op_text for string in tsv)
+    if submission and tsv_match:
+        print ('Title:'), submission.title
+        print ('Body: '), submission.selftext
+        print '---------------------------------\n'
+        print ('WHY U NO WORK')
+    
